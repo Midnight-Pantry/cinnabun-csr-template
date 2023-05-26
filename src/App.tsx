fbe52d2c72ddc3ffd7e7faf6df841a5d566e260e
Robby6Strings <rausten93@gmail.com>
2023-05-26 17:38:12 +1200

    simplified template

--- src/App.tsx
+++ src/App.tsx
@@ -1,56 +1,25 @@
-import { createSignal } from "cinnabun"
-import { Link, Route, Router } from "cinnabun/router"
-import {
-  SignalsExample,
-  ContextExample,
-  SuspenseExample,
-  NestedRoutingExample,
-  ToDoExample,
-} from "./components"
-
-const pathStore = createSignal(window.location.pathname)
+import { Logo } from "./Logo"
 
 export const App = () => {
   return (
     <div style={{ display: "flex", minHeight: "100vh" }}>
       <h1>Cinnabun JS</h1>
       <br />
-      <nav>
-        <ul>
-          <li>
-            <Link to="/" innerText="Signals" store={pathStore} />
-          </li>
-          <li>
-            <Link to="/context" innerText="Context" store={pathStore} />
-          </li>
-          <li>
-            <Link to="/suspense" innerText="Suspense" store={pathStore} />
-          </li>
-          <li>
-            <Link
-              to="/nested-routing"
-              innerText="Nested Routing"
-              store={pathStore}
-            />
-          </li>
-          <li>
-            <Link to="/todo" innerText="ToDo" store={pathStore} />
-          </li>
-        </ul>
-      </nav>
-
-      <main style={{ textAlign: "center", flexGrow: "1" }}>
-        <Router store={pathStore}>
-          <Route path="/" component={<SignalsExample />} />
-          <Route path="/context" component={<ContextExample />} />
-          <Route path="/suspense" component={<SuspenseExample cache />} />
-          <Route
-            path="/nested-routing"
-            component={<NestedRoutingExample {...{ pathStore }} />}
-          />
-          <Route path="/todo" component={<ToDoExample />} />
-        </Router>
-      </main>
+      <Logo />
+      <br />
+      <h2>Get started by editing App.tsx!</h2>
+      <footer>
+        <span style={{ color: "#ccc" }}>Made with love and friends 💞</span>
+        <a target="_new" href="https://github.com/Midnight-Pantry/cinnabun">
+          Github
+        </a>
+        <a
+          target="_new"
+          href="https://github.com/Midnight-Pantry/cinnabun/tree/main/apps/components/src"
+        >
+          Component Examples
+        </a>
+      </footer>
     </div>
   )
 }
