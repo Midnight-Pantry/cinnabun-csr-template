d6b9f6644e7d63011970d0f29efa68e738cd97a4
Robby6Strings <rausten93@gmail.com>
2023-06-16 23:48:57 +1200

    ^version

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,27 @@
 import { createSignal } from "cinnabun"
+import { Router, Route, Link } from "cinnabun/router"
 import { Logo } from "./Logo"
+import { pathStore } from "./state"
+
+const Navigation = () => (
+  <nav>
+    <ul>
+      <li>
+        <Link to="/" store={pathStore}>
+          Home
+        </Link>
+      </li>
+      <li>
+        <Link to="/counter" store={pathStore}>
+          Counter
+        </Link>
+      </li>
+    </ul>
+  </nav>
+)
 
 const Counter = () => {
   const count = createSignal(0)
-
   return (
     <>
       <h2>{count}</h2>
@@ -20,7 +38,13 @@ export const App = () => {
       <Logo />
       <br />
       <h2>Get started by editing App.tsx!</h2>
-      <Counter />
+
+      <Navigation />
+      <Router store={pathStore}>
+        <Route path="/" component={<h3>Home</h3>} />
+        <Route path="/counter" component={<Counter />} />
+      </Router>
+
       <footer>
         <span style={{ color: "#ccc" }}>Made with love and friends 💞</span>
         <a target="_new" href="https://github.com/Midnight-Pantry/cinnabun">
